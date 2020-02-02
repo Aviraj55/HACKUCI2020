b39a9343ebc0140638b52c775cf78505ecef45cb
Edward <edwardzhang623@gmail.com>
2020-02-02 12:30:35 -0800

    fix space

--- app/clean_advertisement.py
+++ app/clean_advertisement.py
@@ -5,7 +5,12 @@ def clean_ads(recipes_dict):
     regex_base = re.compile("ADVERTISEMENT")
     for recipe, rec_attributes in recipes_dict.items():
         for i in range(len(rec_attributes["ingredients"])):
-            if regex_base.sub("", rec_attributes["ingredients"][i]):
-                rec_attributes["ingredients"][i] = regex_base.sub("", rec_attributes["ingredients"][i])
-            else:
+            # if rec_attributes["ingredients"][i] == 'INGRDIENTS':
+            #     del rec_attributes["ingredients"][i]
+            #     continue
+            if regex_base.sub(" ", rec_attributes["ingredients"][i]):
+                rec_attributes["ingredients"][i] = regex_base.sub(" ", rec_attributes["ingredients"][i])
+        for i in range(len(rec_attributes["ingredients"])):
+            if rec_attributes["ingredients"][i] == ' ':
                 del rec_attributes["ingredients"][i]
+                
