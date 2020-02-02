7f970c1b2774d9cbad5fe8749cf154d6f8ab6b32
Edward <edwardzhang623@gmail.com>
2020-02-01 17:26:35 -0800

    misc

--- app/app.py
+++ app/app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 app.secret_key = os.urandom(12)  # Generic key for dev purposes only
 
 # Heroku
-#from flask_heroku import Heroku
-#heroku = Heroku(app)
+# from flask_heroku import Heroku
+# heroku = Heroku(app)
 
 INGREDIENTS_LIST = []
 
@@ -33,76 +33,6 @@ def home():
         return redirect(url_for('results'))
     return render_template('home.html', form=form)
 
-
-example = {
-  "rmK12Uau.ntP510KeImX506H6Mr6jTu": {
-    "title": "Slow Cooker Chicken and Dumplings",
-    "ingredients": [
-      "4 skinless, boneless chicken breast halves ADVERTISEMENT",
-      "2 tablespoons butter ADVERTISEMENT",
-      "2 (10.75 ounce) cans condensed cream of chicken soup ADVERTISEMENT",
-      "1 onion, finely diced ADVERTISEMENT",
-      "2 (10 ounce) packages refrigerated biscuit dough, torn into pieces ADVERTISEMENT",
-      "ADVERTISEMENT"
-    ],
-    "instructions": "Place the chicken, butter, soup, and onion in a slow cooker, and fill with enough water to cover.\nCover, and cook for 5 to 6 hours on High. About 30 minutes before serving, place the torn biscuit dough in the slow cooker. Cook until the dough is no longer raw in the center.\n",
-    "picture_link": "55lznCYBbs2mT8BTx6BTkLhynGHzM.S"
-  },
-  "rmK12Uau.ntP510KeImX506H6Mr6Tu": {
-    "title": "Chicken and Dumplings",
-    "ingredients": [
-      "4 skinless, boneless chicken breast halves ADVERTISEMENT",
-      "2 tablespoons butter ADVERTISEMENT",
-      "2 (10.75 ounce) cans condensed cream of chicken soup ADVERTISEMENT",
-      "1 onion, finely diced ADVERTISEMENT",
-      "2 (10 ounce) packages refrigerated biscuit dough, torn into pieces ADVERTISEMENT",
-      "ADVERTISEMENT"
-    ],
-    "instructions": "Place the chicken, butter, soup, and onion in a slow cooker, and fill with enough water to cover.\nCover, and cook for 5 to 6 hours on High. About 30 minutes before serving, place the torn biscuit dough in the slow cooker. Cook until the dough is no longer raw in the center.\n",
-    "picture_link": "55lznCYBbs2mT8BTx6BTkLhynGHzM.S"
-  },
-  "rmK12Uau.ntP510KeImX506H6Mr6Tud": {
-    "title": "Chicken and Dumplings",
-    "ingredients": [
-      "4 skinless, boneless chicken breast halves ADVERTISEMENT",
-      "2 tablespoons butter ADVERTISEMENT",
-      "2 (10.75 ounce) cans condensed cream of chicken soup ADVERTISEMENT",
-      "1 onion, finely diced ADVERTISEMENT",
-      "2 (10 ounce) packages refrigerated biscuit dough, torn into pieces ADVERTISEMENT",
-      "ADVERTISEMENT"
-    ],
-    "instructions": "Place the chicken, butter, soup, and onion in a slow cooker, and fill with enough water to cover.\nCover, and cook for 5 to 6 hours on High. About 30 minutes before serving, place the torn biscuit dough in the slow cooker. Cook until the dough is no longer raw in the center.\n",
-    "picture_link": "55lznCYBbs2mT8BTx6BTkLhynGHzM.S"
-  },
-  "rmK12Uau.ntP510KeIddmX506H6Mr6Tu": {
-    "title": "Chicken and Dumplings",
-    "ingredients": [
-      "4 skinless, boneless chicken breast halves ADVERTISEMENT",
-      "2 tablespoons butter ADVERTISEMENT",
-      "2 (10.75 ounce) cans condensed cream of chicken soup ADVERTISEMENT",
-      "1 onion, finely diced ADVERTISEMENT",
-      "2 (10 ounce) packages refrigerated biscuit dough, torn into pieces ADVERTISEMENT",
-      "ADVERTISEMENT"
-    ],
-    "instructions": "Place the chicken, butter, soup, and onion in a slow cooker, and fill with enough water to cover.\nCover, and cook for 5 to 6 hours on High. About 30 minutes before serving, place the torn biscuit dough in the slow cooker. Cook until the dough is no longer raw in the center.\n",
-    "picture_link": "55lznCYBbs2mT8BTx6BTkLhynGHzM.S"
-  },
-  "rmK12Uau.ntP510KdsdeImX506H6Mr6Tu": {
-    "title": "Chicken and Dumplings",
-    "ingredients": [
-      "4 skinless, boneless chicken breast halves ADVERTISEMENT",
-      "2 tablespoons butter ADVERTISEMENT",
-      "2 (10.75 ounce) cans condensed cream of chicken soup ADVERTISEMENT",
-      "1 onion, finely diced ADVERTISEMENT",
-      "2 (10 ounce) packages refrigerated biscuit dough, torn into pieces ADVERTISEMENT",
-      "ADVERTISEMENT"
-    ],
-    "instructions": "Place the chicken, butter, soup, and onion in a slow cooker, and fill with enough water to cover.\nCover, and cook for 5 to 6 hours on High. About 30 minutes before serving, place the torn biscuit dough in the slow cooker. Cook until the dough is no longer raw in the center.\n",
-    "picture_link": "55lznCYBbs2mT8BTx6BTkLhynGHzM.S"
-  }
-}
-
-
 @app.route("/results")
 def results():
     plain_json = get_all_valid_recipes(INGREDIENTS_LIST)
@@ -111,8 +41,12 @@ def results():
     #clean_ads(example)
     #print(type(example))
     #print(type(plain_json))
+    if len(INGREDIENTS_LIST) < 7:
+        return render_template('error.html')
     return render_template('results.html', recipes=plain_json)
 
-
+@app.route("/error")
+def error():
+    return render_template('error.html')
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, host="0.0.0.0")
--- app/clean_advertisement.py
+++ app/clean_advertisement.py
@@ -5,4 +5,7 @@ def clean_ads(recipes_dict):
     regex_base = re.compile("ADVERTISEMENT")
     for recipe, rec_attributes in recipes_dict.items():
         for i in range(len(rec_attributes["ingredients"])):
-            rec_attributes["ingredients"][i] = regex_base.sub("", rec_attributes["ingredients"][i])
+            if regex_base.sub("", rec_attributes["ingredients"][i]):
+                rec_attributes["ingredients"][i] = regex_base.sub("", rec_attributes["ingredients"][i])
+            else:
+                del rec_attributes["ingredients"][i]
